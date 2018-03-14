f2b98bba8df0f409f6ca53fbe21c863e037b3120
Arthur Liebhardt <liebhardt.arthur@gmail.com>
2018-03-15 00:36:13 +0100

    Serverless functions for sendReminderDaily and sendReminderWeekend

--- serverless-reminders/handler.js
+++ serverless-reminders/handler.js
@@ -1,16 +1,83 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+module.exports.sendReminderDaily = (event, context, callback) => {
+  var AWS = require('aws-sdk');
+  AWS.config.update({ region: 'us-east-1' });
+  var ses = new AWS.SES();
+  var fs = require('fs');
+
+  var emailHtml = fs.readFileSync('./dailyReminder.html', 'utf-8');
+
+  var toAndFromAddress = 'cryptoatr4ik@gmail.com';
+  var params = {
+    Destination: {
+      ToAddresses: [toAndFromAddress]
+    },
+    Message: {
+      Body: {
+        Html: {
+          Charset: 'UTF-8',
+          Data: emailHtml
+        },
+        Text: {
+          Charset: "UTF-8",
+          Data: "Remember to continue helping the Woof Garden!"
+        }
+      },
+      Subject: {
+        Charset: "UTF-8",
+        Data: "Woof Garden Reminder"
+      }
+    },
+    ReplyToAddresses: [toAndFromAddress],
+    Source: toAndFromAddress
   };
 
-  callback(null, response);
+  ses.sendEmail(params, (err, data) => {
+    //an error occurred
+    if (err) console.log(err, err.stack);
+    // successful response
+    else callback(null, data);
+  });
+};
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+module.exports.sendReminderWeekend = (event, context, callback) => {
+  var AWS = require('aws-sdk');
+  AWS.config.update({ region: 'us-east-1' });
+  var ses = new AWS.SES();
+  var fs = require('fs');
+
+  var emailHtml = fs.readFileSync('./weekendReminder.html', 'utf-8');
+  var toAndFromAdress = 'cryptoatr4ik@gmail.com';
+  var params = {
+    Destination: {
+      ToAddresses: [toAndFromAdress]
+    },
+    Message: {
+      Body: {
+        Html: {
+          Charset: "UTF-8",
+          Data: emailHtml
+        },
+        Text: {
+          Charset: "UTF-8",
+          Data: "Here's a weekend Remember that puppies are adorable!!"
+        }
+      },
+      Subject: {
+        Charset: "UTF-8",
+        Data: "Woof Garden Reminder"
+      }
+    },
+    ReplyToAddresses: [toAndFromAdress],
+    Source: toAndFromAdress,
+  };
+
+  ses.sendEmail(params, function (err, data) {
+    // an error occurred
+    if (err) console.log(err, err.stack);
+    // successful response
+    else callback(null, data);
+  });
+
 };
