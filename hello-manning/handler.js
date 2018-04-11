c1690c64146c622975d51778312e8942120c0cab
Arthur Liebhardt <liebhardt.arthur@gmail.com>
2018-04-11 20:30:13 +0200

    hola handler

--- hello-manning/handler.js
+++ hello-manning/handler.js
@@ -13,3 +13,17 @@ module.exports.hello = (event, context, callback) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
+
+module.exports.hola = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'hola chicka',
+    }),
+  };
+
+  callback(null, response);
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
