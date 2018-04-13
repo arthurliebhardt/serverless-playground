31b8d0da2ce939f2e2522dafdf1a36dff6a94427
Arthur Liebhardt <liebhardt.arthur@gmail.com>
2018-04-13 23:16:52 +0200

    added integration tests

--- big-mouth/functions/get-index.js
+++ big-mouth/functions/get-index.js
@@ -12,7 +12,7 @@ const awsRegion = process.env.AWS_REGION;
 const cognitoUserPoolId = process.env.cognito_user_pool_id;
 const cognitoClientId = process.env.cognito_client_id;
 
-const restaurantsApiRoot = process.env.restaraunts_api
+const restaurantsApiRoot = process.env.restaurants_api
 const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
 
 var html;
@@ -33,14 +33,18 @@ function* getRestaurants() {
   };
 
   aws4.sign(opts);
-  
-  return (yield http
+
+  let httpReq = http
   .get(restaurantsApiRoot)
   .set('Host', opts.headers['Host'])
   .set('X-Amz-Date', opts.headers['X-Amz-Date'])
-  .set('Authorization', opts.headers['Authorization'])
-  .set('X-Amz-Security-Token', opts.headers['X-Amz-Security-Token'])
-  ).body;
+  .set('Authorization', opts.headers['Authorization']);
+
+  if(opts.headers['X-Amz-Security-Token']) {
+    httpReq.set('X-Amz-Security-Token', opts.headers['X-Amz-Security-Token']);
+  }
+  
+  return (yield httpReq).body;
 }
 
 module.exports.handler = co.wrap(function* (event, context, callback) {
