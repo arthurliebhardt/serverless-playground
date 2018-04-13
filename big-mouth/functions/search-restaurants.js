4021c79d1c92f66c2599e91c93cfaa2e795ac761
Arthur Liebhardt <liebhardt.arthur@gmail.com>
2018-04-13 21:48:49 +0200

    hand an error in searching due to typo

--- big-mouth/functions/search-restaurants.js
+++ big-mouth/functions/search-restaurants.js
@@ -7,17 +7,18 @@ const dynamodb = new AWS.DynamoDB.DocumentClient();
 const defaultResults = process.env.defaultResults || 8;
 const tableName = process.env.restaurants_table
 
-function* findRestaurantsByTheme(count) {
+function* findRestaurantsByTheme(theme, count) {
     let req = {
         TableName: tableName,
         Limit: count,
         FilterExpression: "contains(themes, :theme)",
-        ExpressionAttributeValues: { ":theme": theme}
+        ExpressionAttributeValues: { ":theme": theme }
     };
 
     let resp = yield dynamodb.scan(req).promise();
     return resp.Items;
 }
+
 
 module.exports.handler = co.wrap(function* (event, context, cb) {
     let req = JSON.parse(event.body);
@@ -28,5 +29,4 @@ module.exports.handler = co.wrap(function* (event, context, cb) {
     }
 
     cb(null, response);
-
-})
+});
