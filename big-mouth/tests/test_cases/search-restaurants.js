06cbe3067cac11d291d0db470f5fddf0a57909dc
Arthur Liebhardt <liebhardt.arthur@gmail.com>
2018-04-15 17:47:00 +0200

    Acceptance tests

--- big-mouth/tests/test_cases/search-restaurants.js
+++ big-mouth/tests/test_cases/search-restaurants.js
@@ -4,22 +4,30 @@ const co = require('co');
 const expect = require('chai').expect;
 const init = require('../steps/init').init;
 const when = require('../steps/when');
+const given = require('../steps/given');
 
-
-describe(`When we invoke the POST /restaurants/search endpoint with theme 'cartoon'`, co.wrap(function* () {
+describe(`Given an authenticated user`, co.wrap(function* () {
+    let user;
     before(co.wrap(function* () {
-      yield init();
+        yield init();
+        user = yield given.an_authenticated_user();
     }));
 
-    it(`Should return an array of 4 restaurants`, co.wrap(function* () {
-        let res = yield when.we_invoke_search_restaurants('cartoon');
-    
-        expect(res.statusCode).to.equal(200);
-        expect(res.body).to.have.lengthOf(4);
-    
-        for (let restaurant of res.body) {
-          expect(restaurant).to.have.property('name');
-          expect(restaurant).to.have.property('image');
-        }
-      }));
+    describe(`When we invoke the POST /restaurants/search endpoint with theme 'cartoon'`, co.wrap(function* () {
+        before(co.wrap(function* () {
+            yield init();
+        }));
+
+        it(`Should return an array of 4 restaurants`, co.wrap(function* () {
+            let res = yield when.we_invoke_search_restaurants('cartoon');
+
+            expect(res.statusCode).to.equal(200);
+            expect(res.body).to.have.lengthOf(4);
+
+            for (let restaurant of res.body) {
+                expect(restaurant).to.have.property('name');
+                expect(restaurant).to.have.property('image');
+            }
+        }));
+    }));
 }));
